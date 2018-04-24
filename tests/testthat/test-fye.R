421330fac165eb8cd22c825f50e6501c23c2c476
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2018-04-25 09:03:39 +1000

    add test to the FYE function for the type of the collstr and dP values

--- tests/testthat/test-fye.R
+++ tests/testthat/test-fye.R
@@ -9,6 +9,21 @@ out <- colloc_leipzig(leipzig_corpus_list = demo_corpus_leipzig,
 assoc_tb <- assoc_prepare(colloc_out = out, stopword_list = stopwords)
 am_fye <- collex_fye(df = assoc_tb, collstr_digit = 3)
 
+test_that("collex_fye produces tibble", {
+  expect_output(str(am_fye), "tbl_df")
+})
+
 test_that("collex_fye produces tibble of 7 columns", {
   expect_equal(dim(am_fye)[2], 7L)
+})
+
+test_that("collstr and the dPs measures are of the type 'double'", {
+  expect_type(am_fye$collstr, "double")
+  expect_type(am_fye$dP_collex_cue_cxn, "double")
+  expect_type(am_fye$dP_cxn_cue_collex, "double")
+})
+
+collstr_vals <- c(Inf, 12.2, 5.42)
+test_that("'Inf' and non-infinite collstr are of the type 'double'", {
+  expect_type(collstr_vals, "double")
 })
